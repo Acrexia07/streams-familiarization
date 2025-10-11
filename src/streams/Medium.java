049760e3e78aa1213596a2cd213a4d0ab5fb6c92
Acrexia07 <barnuevo.marlon07@gmail.com>
2025-10-12 04:49:46 +0800

    feat: added person object and complete hard problems

--- src/streams/Medium.java
+++ src/streams/Medium.java
@@ -4,5 +4,8 @@ public class Medium {
 
     public static void streamsMedium() {
         System.out.println("*--- Streams for Medium category ---*");
+
+        System.out.println("OCTOBER 12, 2025");
+        System.out.println("Problem: Get the sum of even numbers");
     }
 }
